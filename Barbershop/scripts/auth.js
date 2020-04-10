d668e995a278f89cc734fe74be0c531747356730
Eoin Kirwan <eoinkirwan@Eoins-MacBook-Pro.local>
2020-04-10 02:31:36 +0100

    Added new security features, create reviews

--- Barbershop/scripts/auth.js
+++ Barbershop/scripts/auth.js
@@ -1,3 +1,43 @@
+auth.onAuthStateChanged(user => {
+  if (user) {
+    db.collection('reviews').onSnapshot(snapshot => {
+      setupReviews(snapshot.docs);
+      console.log(user);
+      displayUI(user);
+    }, err => console.log(err.message));
+  } else {
+    displayUI();
+    setupReviews([]);
+  }
+});
+
+
+
+//Creating new reviews
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit' , (e) =>{
+  e.preventDefault();
+  
+  db.collection('reviews').add({
+    
+    title: createForm['title'].value,
+    content: createForm['content'].value,
+    
+    
+
+  }).then(() =>{
+    //close the modal and set the form fieleds to null
+
+    const modal = document.querySelector('#modal-create');
+    M.Modal.getInstance(modal).close();
+    createForm.reset();
+  }).catch(err =>{
+    console.log(err.message);
+  })
+})
+
+
+
 //Create account
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -8,11 +48,46 @@ signupForm.addEventListener('submit', (e) => {
   const password = signupForm['signup-password'].value;
 
     //Register user
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
-        const modal = document.querySelector('#modal-signup');
-        MSAssertion.Modal.getInstance(modal.close());
+    auth.createUserWithEmailAndPassword(email, password).then(function (result) {
+      return result.user.updateProfile({
+        displayName: signupForm['signup-username'].value,
+        
+        
+      })
+        
+    }).then(() =>{
+      const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
         signupForm.reset();
-    })
+        
+    });
     //.then method fires a call back function when user is registered
+})
+
+//Logout method
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut();
+})
+
+
+//Login method
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e)=>{
+  e.preventDefault();
+
+  //Gets user creds from login form
+
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+
+
+    //closing the login modal and clear form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  })
 })
--- Barbershop/scripts/index.js
+++ Barbershop/scripts/index.js
@@ -1,3 +1,50 @@
+const reviewList = document.querySelector('.reviews');
+const userLoggedOutLinks = document.querySelectorAll('.logged-out');
+const userLoggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails = document.querySelector('.account-details');
+
+const displayUI = (user =>{
+  if(user){ //display logged in elements to logged in users
+    //User details
+    const html =`
+    <div> Logged in as ${user.displayName}</div>
+    `;
+    accountDetails.innerHTML =html
+    //display UI elements
+    userLoggedInLinks.forEach(item =>item.style.display = 'block')
+    userLoggedOutLinks.forEach(item =>item.style.display = 'none') 
+  }
+  else{//display logged out elements to logged out users
+    //Hide account details when user logs out
+    accountDetails.innerHTML = '';
+    //display UI elements
+    userLoggedInLinks.forEach(item =>item.style.display = 'none')
+    userLoggedOutLinks.forEach(item =>item.style.display = 'block') 
+  }
+})
+
+//sets up reviews
+const setupReviews = (data) => {
+  if(data.length){
+    let html = '';
+    data.forEach(doc =>{
+      const review = doc.data();
+      const li = `
+      <li>
+        <div class = "collapsible-header grey lighten-4">${review.title}</div>
+        <div class ="collapsible-body white">${review.content}</div>
+      </li>
+      `;
+      html+= li
+    });
+    reviewList.innerHTML = html;
+  }
+  else{//If user is not login, display this message
+    reviewList.innerHTML= '<h5 class="center-align"> Login to view reviews</h5>'
+  } 
+}
+
+//Sets up the material components
 document.addEventListener('DOMContentLoaded', function() {
 
   var modals = document.querySelectorAll('.modal');
@@ -7,3 +54,4 @@ document.addEventListener('DOMContentLoaded', function() {
   M.Collapsible.init(items);
 
 });
+
